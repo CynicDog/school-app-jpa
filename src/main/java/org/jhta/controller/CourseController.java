d26d1a188d42c9f5be2a20bee3c22cff6a352a55
ginsenglee <thecynicdog0328@gmail.com>
2023-05-11 12:38:00 +0900

    course controller

---
+++ src/main/java/org/jhta/controller/CourseController.java
@@ -0,0 +1,13 @@
+package org.jhta.controller;
+
+public class CourseController {
+
+}
+
+// openUpCourse
+// lookUpCoursesById
+// getCourseById
+// lookUpCourses
+// applyCourse
+// lookUpRegisteredStudentsById
+// lookUpRegisteredCoursesById
