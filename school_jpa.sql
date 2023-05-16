510f27791489b47f4228e162ae0734be4def4854
ginsenglee <thecynicdog0328@gmail.com>
2023-05-16 12:21:04 +0900

    create database

--- school_jpa.sql
+++ school_jpa.sql
@@ -1,7 +1,7 @@
 /* Create Database*/
-DROP DATABASE IF EXISTS school_jpa;
-CREATE DATABASE school_jpa;
-USE school_jpa;
+DROP DATABASE IF EXISTS school_app_jpa;
+CREATE DATABASE school_app_jpa;
+USE school_app_jpa;
 
 DROP DATABASE IF EXISTS school_jpa_test;
 CREATE DATABASE school_jpa_test;
